0e9bd619657bcd7dbb0a2408132b7f7a38c22606
unknown <bishtchandan023@gmail.com>
2023-07-11 12:02:12 +0530

    Updated and created responsive app using js

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ window.onload = function () {
   const car = document.querySelector(".car");
   const track = document.querySelector(".track");
   const wheel = document.querySelectorAll(".wheel>img");
-  let mobileVersion = window.matchMedia("(max-width: 600px)");
+  const mobileVersion = window.matchMedia("(max-width: 600px)");
+  const tabletVersion = window.matchMedia("(max-width: 992px)");
+  const laptopVersion = window.matchMedia("(max-width: 1024px)");
 
   const audio = document.createElement("audio");
   audio.setAttribute("src", "sound.mp3");
@@ -15,16 +17,32 @@ window.onload = function () {
     for (let i = 0; i < wheel.length; i++) {
       wheel[i].style.animation = "wheelRotation linear .16s infinite";
     }
-    track.style.animation = "carMove linear 6s infinite";
-    trees.style.animation = "shakebody linear 6s infinite";
     car.style.animation = "carShake linear 3s infinite";
-    mobileVersion.addEventListener("change", function () {
-      if (mobileVersion.matches) {
-        car.style.left = "40px";
+    function mediaMatch(x, y, z) {
+      if (x.matches) {
+        car.style.left = "0";
+        track.style.animation = "carMove linear 2s infinite";
+        trees.style.animation = "shakebody linear 2s infinite";
+      } else if (y.matches) {
+        car.style.left = "200px";
+        track.style.animation = "carMove linear 4s infinite";
+        trees.style.animation = "shakebody linear 4s infinite";
+      } else if (z.matches) {
+        car.style.left = "130px";
+        track.style.animation = "carMove linear 6s infinite";
+        trees.style.animation = "shakebody linear 6s infinite";
       } else {
         car.style.left = "333px";
+        track.style.animation = "carMove linear 6s infinite";
+        trees.style.animation = "shakebody linear 6s infinite";
       }
-    });
+    }
+    setInterval(() => {
+      mediaMatch(mobileVersion, tabletVersion, laptopVersion);
+      mobileVersion.addEventListener("change", mediaMatch);
+      tabletVersion.addEventListener("change", mediaMatch);
+      laptopVersion.addEventListener("change", mediaMatch);
+    }, 0);
     audio.loop = true;
     audio.play();
   });
